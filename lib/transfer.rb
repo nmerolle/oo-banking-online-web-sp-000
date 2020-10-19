8d20e67406647a1ed25bef6bf9da1a977177cdaa
Nicholas Merolle <nmerolle11@hotmail.com>
2020-10-19 18:43:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,7 +21,7 @@ class Transfer
       @status = "complete"
     else
       @status = "rejected"
-      puts "Transaction rejected. Please check your account balance."
+      "Transaction rejected. Please check your account balance."
     end
   end
 
