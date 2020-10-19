1861549812928ccfee117236576d13abe153f618
Nicholas Merolle <nmerolle11@hotmail.com>
2020-10-19 16:31:34 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -25,12 +25,9 @@ class BankAccount
   end
 
   def valid?
-    (!!status == 'open') && (!!balance > 0)
+    (status == 'open') && (balance > 0)
   end
 
-  def account_hash
-    instance_variables.map do |var|
-        [var[1..-1].to_sym, instance_variable_get(var)]
-    end.to_h
+
   end
 end
