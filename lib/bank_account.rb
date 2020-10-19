d2372fd580713add3f705a23a08eebfcd21e1731
Nicholas Merolle <nmerolle11@hotmail.com>
2020-10-19 16:27:27 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -8,5 +8,29 @@ class BankAccount
     @status = status
   end
   
-  
+    def deposit(deposit_amount)
+    @balance += deposit_amount
+  end
+
+  def display_balance
+    p "Your balance is $#{self.balance}."
+  end
+
+  def status=(status)
+    @status
+  end
+
+  def balance=(balance)
+    @balance
+  end
+
+  def valid?
+    (!!status == 'open') && (!!balance > 0)
+  end
+
+def account_hash
+    instance_variables.map do |var|
+        [var[1..-1].to_sym, instance_variable_get(var)]
+    end.to_h
+  end
 end
