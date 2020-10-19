0e0f479e4877f9c9e1cbacd75763fe61cab61eca
Nicholas Merolle <nmerolle11@hotmail.com>
2020-10-19 18:42:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance > @amount && @status == "pending" && valid? == true
+    if @sender.balance > @amount && @status == "pending" && receiver.valid? == true
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
