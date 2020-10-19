6f295a32690d3bc833f7a0690bf4bb5d1b796b5d
Nicholas Merolle <nmerolle11@hotmail.com>
2020-10-19 18:07:02 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -5,10 +5,10 @@ class BankAccount
   attr_reader :name
   
   
-  def initialize(name, balance = 1000, status = "open")
+  def initialize(name)
     @name = name
-    @balance = balance
-    @status = status
+    @balance = 1000
+    @status = "open"
   end
   
   def deposit(deposit_amount)
@@ -28,8 +28,7 @@ class BankAccount
   end
 
   def valid?
-    binding.pry
-   status == "open" && balance > 0 ? true : false
+    status == "open" && balance > 0 ? true : false
   end
     
   def close_account
